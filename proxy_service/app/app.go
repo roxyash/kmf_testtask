76cf882b865b7da71b5e618715b7b1741dbf8154
roxyash <roxyash@mail.ru>
2023-02-18 16:30:07 +0600

    add new dir app, with initialize all app in other dir, and call app.New in main.app

---
+++ proxy_service/app/app.go
@@ -0,0 +1,69 @@
+package app
+
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"github.com/roxyash/kmf_testtask/pkg/zaplogger"
+	"github.com/roxyash/kmf_testtask/proxy_service/config"
+	"github.com/roxyash/kmf_testtask/proxy_service/internal/handler"
+	"github.com/roxyash/kmf_testtask/proxy_service/internal/repository"
+	"github.com/roxyash/kmf_testtask/proxy_service/internal/route"
+	"github.com/roxyash/kmf_testtask/proxy_service/internal/service"
+	httpserver "github.com/roxyash/kmf_testtask/proxy_service/server/http"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type App struct {
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+func (a *App) Run() {
+	log.Println("Initialize config . . .")
+	if err := config.InitConfig("proxy_service/config", "local"); err != nil {
+		log.Fatalf("Error initialize config: %s", err.Error())
+	}
+
+	log.Println("Initialize logger . . .")
+	logger := zaplogger.NewZapLogger(viper.GetString("app.logPath"), "")
+
+	logger.Infof("Initialize env files. . .")
+	if err := godotenv.Load(viper.GetString("app.envPath")); err != nil {
+		logger.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	logger.Infof("Initialize packages of service . . .")
+
+	repo := repository.NewRepository()
+
+	services := service.NewService(repo)
+
+	handlers := handler.NewHandler(logger, services)
+
+	routes := route.NewRoute(handlers)
+
+	srv := new(httpserver.Server)
+
+	go func() {
+		if err := srv.Run(viper.GetString("app.port"), routes.InitRoutes()); err != nil {
+			logger.Fatalf("error occurred while running http server: %s", err.Error())
+		}
+	}()
+
+	logger.Infof("%v on http server, port: %v,  Started", viper.GetString("app.serviceName"), viper.GetString("app.port"))
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logger.Infof("%v on http server, port: %v ShuttingDown", viper.GetString("app.serviceName"), viper.GetString("app.port"))
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logger.Fatalf("error occurred on server shutting down :%s", err.Error())
+	}
+}
